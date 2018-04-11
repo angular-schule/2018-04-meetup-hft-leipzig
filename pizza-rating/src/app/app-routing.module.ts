253c7841669faaf8c9f7d6e767f5002ff1f4965c
Ferdinand Malcher <mail@ferdinand-malcher.de>
2018-04-11 20:56:01 +0200

    Routing: Routenkonfig und RouterOutlet

--- pizza-rating/src/app/app-routing.module.ts
+++ pizza-rating/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PizzaDetailsComponent } from './pizza-details/pizza-details.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'pizza/:id', component: PizzaDetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- pizza-rating/src/app/app.module.ts
+++ pizza-rating/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { PizzaComponent } from './pizza/pizza.component';
 import { PizzaStoreService } from './shared/pizza-store.service';
 import { HttpClientModule } from '@angular/common/http';
+import { PizzaDetailsComponent } from './pizza-details/pizza-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    PizzaComponent
+    PizzaComponent,
+    PizzaDetailsComponent
   ],
   imports: [
     BrowserModule,
